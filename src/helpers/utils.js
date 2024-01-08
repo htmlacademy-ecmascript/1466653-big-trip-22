1fa78b0da9097999d4756162e3d48d0fe706dee0
Goulshat <76894691+Goulshat@users.noreply.github.com>
2024-01-08 13:52:14 +0500

    добавляет сортировку события по длительности и стоимости

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -5,6 +5,15 @@ import dayjs from 'dayjs';
 const isEscapeKey = (evt) => evt.key === 'Escape';
 const updateItem = (items, update) => items.map((item) => item.id === update.id ? update : item);
 
+/* ---- SORTING ----- */
+const sortEventsByPrice = (previousEvent, nextEvent) => (nextEvent.basePrice - previousEvent.basePrice);
+const sortEventsByTime = (previousEvent, nextEvent) => {
+  const previousEventDuration = dayjs(previousEvent.dateFrom).diff((previousEvent.dateTo));
+  const nextEventDuration = dayjs(nextEvent.dateFrom).diff((nextEvent.dateTo));
+
+  return (nextEventDuration - previousEventDuration);
+};
+
 /* ---- RANDOM ELEMENTS ---- */
 const getRandomInteger = (min, max) => {
   const rand = min + Math.random() * (max + 1 - min);
@@ -60,6 +69,8 @@ function getDuration(dateStart, dateEnd) {
 export {
   isEscapeKey,
   updateItem,
+  sortEventsByPrice,
+  sortEventsByTime,
   getRandomInteger,
   getRandomArrayElement,
   getRandomStartDate,
